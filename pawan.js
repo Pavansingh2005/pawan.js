6a405e3885c6633e79d120027079ab45f7e1f669
Pavansingh2005 <pavan.kumar132903@marwadiuniversity.ac.in>
2025-08-04 11:02:53 +0530

    Add files via upload

---
+++ pawan.js
@@ -0,0 +1,22 @@
+const accountid = '2005';
+let accountemail = "pawan@email.com";
+var accountpasssword = "pawan123";
+accountcity = "muzzaffarpur";
+var accountstat;
+
+// accountid =32  //
+
+accountemail ="paan@hc.com";
+accountpasssword = "pawan@123";
+accountcity = "delhi";
+
+
+console.log(accountid);
+
+/*
+prefer not use var
+brcause of issue in block scope and function scope
+*/
+
+
+console.table([accountid , accountemail , accountpasssword, accountcity]);
---
+++ pawan1.js
@@ -0,0 +1,22 @@
+const accountid ="7238";
+let aacountemail ="pawan@jdf.com";
+var accountpassword = "pawan@1234";
+accountcity = "patna";
+
+
+//accountid= "76823";//
+
+accountemail ="hj@jjg.cpm";
+accountpassword = "pawan@12345";
+accountcity = "mumbai";
+
+
+console.log(accountid);
+// this is note//
+/*prefer not use var 
+because of isssue in block scope and function scope
+*/
+console.table([accountid, accountemail, accountpassword,accountcity]);
+
+// chai or code part 2//
+
