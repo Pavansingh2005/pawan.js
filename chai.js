4b6aed02f6afd1c3d534014840ecb65f7730fbe7
Pavansingh2005 <pavan.kumar132903@marwadiuniversity.ac.in>
2025-08-04 11:58:38 +0530

    Add files via upload

---
+++ chai.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js code as newer version 
+
+// alert(3+3)// we are using nodejs , not browse
+
+
+console.log(3 +3); //  code readability should br high
+
+console.log("pawan");
+
+let name="pawan";
+let age =20
+let isloggrdIn = false
+let state;
+
+// number => 2 to power 53 something
+// bigint
+//string =>""
+//boolean=> true/false
+//Null => standalone value
+//undefined=>  
+//symbol =>unique
+
+
+//Object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);   //object
+
+
